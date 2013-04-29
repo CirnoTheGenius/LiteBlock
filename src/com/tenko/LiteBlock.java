513bf1201a1afaef559ae877184333c31b9d50cd
CirnoTheGenius <cirnothegenius@aol.com>
2013-04-28 21:05:27 -0700

    Update LiteBlock.java

--- src/com/tenko/LiteBlock.java
+++ src/com/tenko/LiteBlock.java
@@ -14,6 +14,7 @@ public class LiteBlock extends JavaPlugin {
 	@Override
 	public void onEnable(){
 		try {
+			//Probably going to get told that "OMG UR PASSWURDZ AND USR R EXPOSED!1". FYI, port 3306 isn't forwarded and those aren't even associated with one of my credentials :D
 			this.ch = new ConnectionHolder("jdbc:mysql://localhost:3306/liteblock", "tsunko", "tenshihinanawi");
 			this.ch.executeStatement("SELECT VERSION()");
 		} catch (SQLException e){
